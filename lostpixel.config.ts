61ebc74b60eff661351c43096dd4e9b488d1838d
Matthew Holloway <Matthew Holloway>
2024-10-02 18:45:57 +1300

    feat: visual regression debugging

--- lostpixel.config.ts
+++ lostpixel.config.ts
@@ -2,9 +2,9 @@ export const config = {
   histoireShots: {
     histoireUrl: './client/.histoire/dist'
   },
-  imagePathBaseline: './client/.lostpixel/baseline',
-  imagePathCurrent: './client/.lostpixel/current',
-  imagePathDifference: './client/.lostpixel/difference',
+  imagePathBaseline: './client/.lostpixel/baseline/',
+  imagePathCurrent: './client/.lostpixel/current/',
+  imagePathDifference: './client/.lostpixel/difference/',
   generateOnly: true,
   failOnDifference: true
 }
