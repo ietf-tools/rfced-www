afc5f47cce24099e925ccabb1af19150eb87b480
Matthew Holloway <Matthew Holloway>
2024-10-08 13:17:48 +1300

    feat: Sections

---
+++ client/components/SectionHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-blue-100 pt-7 overflow-hidden relative">
+    <div class="absolute pointer-events-none inset-0 overflow-hidden">
+      <GraphicsIETFMotif
+        class="absolute w-[90vw] h-[90vh] -left-[10vw] -top-[20vh]"
+        :opacity="0.04"
+      />
+      <GraphicsIETFMotif
+        class="absolute w-[40vw] h-[40vh] left-[57vw] md:left-[47vw] xl:left-[37vw] top-[17vh]"
+        :opacity="0.04"
+      />
+    </div>
+    <div class="container mx-auto text-white">
+      <slot />
+    </div>
+  </div>
+</template>
---
+++ client/pages/section.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <SectionHeader>
+      <Heading level="1" class="w-full mt-0 mb-3 pl-5 md:p-0 text-balance">
+        Section index page
+      </Heading>
+      <p class="mb-10">
+        A Request for Comments is a publication in a series from the principal
+        technical development and standards-setting bodies for the Internet.
+      </p>
+    </SectionHeader>
+    <div class="container mx-auto mt-10">
+      <div class="grid grid-cols-1 mt-3 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        <Card href="/sdf" heading-level="3" has-cover-link>
+          <template #headingTitle>What is an RFC?</template>
+          <p class="text-base mt-2 text-blue-900 dark:text-white">
+            Short description of the page here if it seems useful to show
+          </p>
+        </Card>
+
+        <Card href="/sdf" heading-level="3" has-cover-link>
+          <template #headingTitle>What are Errata?</template>
+          <p class="text-base mt-2 text-blue-900 dark:text-white">
+            Short description of the page here if it seems useful to show
+          </p>
+        </Card>
+
+        <Card href="/sdf" heading-level="3" has-cover-link>
+          <template #headingTitle>FAQs</template>
+          <p class="text-base mt-2 text-blue-900 dark:text-white">
+            Short description of the page here if it seems useful to show
+          </p>
+        </Card>
+
+        <Card href="/sdf" heading-level="3" has-cover-link>
+          <template #headingTitle>About the RFC Editor</template>
+          <p class="text-base mt-2 text-blue-900 dark:text-white">
+            Short description of the page here if it seems useful to show
+          </p>
+        </Card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
