260af2f3cacb44de8412b0517e559058d4558c92
Matthew Holloway <matthew@holloway.co.nz>
2024-11-12 20:57:12 +0000

    feat: Extract git log metadata about content markdown files (#14)
    * feat: Extract git log metadata about content markdown files
    
    * chore: remove unnecessary .gitignore

--- client/pages/[...slug].vue
+++ client/pages/[...slug].vue
@@ -2,6 +2,10 @@
   <div class="min-h-[100vh]">
     <div class="container mx-auto">
       <Breadcrumbs :breadcrumb-items="breadcrumbItems" />
+      <pre>
+        {{ route.path }}
+        {{ thisContentMetadata }}
+      </pre>
       <ContentDoc />
     </div>
   </div>
@@ -9,11 +13,21 @@
 
 <script setup lang="ts">
 import { startCase } from 'lodash-es'
+import _contentMetadata from '../content-metadata.json'
 import type { BreadcrumbItem } from '~/components/BreadcrumbsTypes'
+import type { ContentMetadata } from '~/scripts/generate-content-metadata'
+
+const contentMetadata: ContentMetadata = _contentMetadata
 
 const route = useRoute()
 
+const thisContentMetadata = contentMetadata[route.path]
+
 const pathParts = route.path.substring(1).split('/')
+
+// const { data: navigation } = await useAsyncData('navigation', () =>
+//   fetchContentNavigation()
+// )
 
 const breadcrumbItems: BreadcrumbItem[] = [
   { url: '/', label: 'Home' },
---
+++ client/scripts/generate-content-metadata.ts
@@ -0,0 +1,57 @@
+import path from 'node:path'
+import fs from 'node:fs'
+
+import { simpleGit } from 'simple-git'
+import { glob } from 'glob'
+
+const __dirname = import.meta.dirname
+const clientPath = path.resolve(__dirname, '..')
+const contentPath = path.resolve(clientPath, 'content')
+
+const contentMarkdownPaths = await glob(path.join(contentPath, '**/*.md'))
+
+const git = simpleGit()
+
+export type ContentMetadata = Record<
+  string, // path within content directory
+  {
+    timestamp: string // timestamp ISO 8601
+  }
+>
+
+const markdownMetadataArray = await Promise.all(
+  contentMarkdownPaths.map(
+    (contentMarkdownPath) =>
+      new Promise<ContentMetadata>((resolve, reject) => {
+        git
+          .log({
+            file: contentMarkdownPath,
+            maxCount: 1,
+            strictDate: true
+          })
+          .then((gitLog) => {
+            if (gitLog.latest?.date) {
+              const relativePath = contentMarkdownPath.substring(contentPath.length).replace(/\.md$/, '')
+              resolve({
+                [relativePath]: { timestamp: gitLog.latest?.date }
+              })
+            } else {
+              reject(
+                `Unable to extract latest Git log time from path ${contentMarkdownPath}`
+              )
+            }
+          })
+      })
+  )
+)
+
+const contentMetadata: ContentMetadata = Object.assign({}, ...markdownMetadataArray)
+
+const contentMetadataPath = path.join(clientPath, 'content-metadata.json')
+
+fs.writeFileSync(contentMetadataPath, JSON.stringify(contentMetadata, null, 2))
+
+console.log(
+  'Success: timestamps extract from git and written to',
+  contentMetadataPath
+)
