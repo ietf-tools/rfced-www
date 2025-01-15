c6fabe0656e313eb4d0c9f202a9096d8a75b24ad
Matthew Holloway <matthew@holloway.co.nz>
2025-01-16 12:34:21 +1300

    feat: Feeds (RSS2, Atom1)

---
+++ client/utilities/feed.ts
@@ -0,0 +1,51 @@
+import { Feed } from 'feed'
+import type { FeedOptions } from 'feed'
+import { ApiClient } from '~/generated/red-client'
+import { PUBLIC_SITE, PRIVATE_API_URL, rfcPathBuilder } from './url'
+
+type DocListArg = Parameters<ApiClient['red']['docList']>[0]
+
+export const getFeedForLatestRFCs = async (
+  numberOfRFCs: number,
+  feedOptions?: FeedOptions
+) => {
+  const redApi = new ApiClient({ baseUrl: PRIVATE_API_URL })
+
+  const docListArg: DocListArg = {}
+
+  docListArg.sort = ['-number'] // sort by latest RFC
+  docListArg.limit = numberOfRFCs
+
+  const response = await redApi.red.docList(docListArg)
+
+  const feed = new Feed({
+    title: 'Recent RFCs',
+    description: 'Recently published RFCs',
+    id: PUBLIC_SITE,
+    link: PUBLIC_SITE,
+    generator: 'https://www.npmjs.com/package/feed',
+    language: 'en-us', // optional, used only in RSS 2.0, possible values: http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
+    copyright: '',
+    ...(feedOptions ?? {}),
+    updated:
+      response.results.length > 0 ?
+        parseDateString(response.results[0].published)
+      : new Date()
+  })
+
+  response.results.forEach((rfcMetadata) => {
+    const url = `${PUBLIC_SITE}${rfcPathBuilder(`RFC${rfcMetadata.number}`)}`
+    feed.addItem({
+      title: `RFC ${rfcMetadata.number}: ${rfcMetadata.title}`,
+      link: url,
+      description: rfcMetadata.abstract,
+      date: parseDateString(rfcMetadata.published)
+    })
+  })
+
+  return feed
+}
+
+const parseDateString = (date: string): Date => {
+  return new Date(date)
+}
