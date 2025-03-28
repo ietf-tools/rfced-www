ec051ceb437f6155f342dbcf1ae3768a4bcadf8f
Matthew Holloway <matthew@staff.ietf.org>
2025-03-28 13:49:39 +1300

    chore: ensuring mailto links aren't opened in a new window or with rel noopener

--- client/components/A.vue
+++ client/components/A.vue
@@ -9,16 +9,21 @@ import { computed } from 'vue'
 import { RouterLink } from '#vue-router'
 import { EXTERNAL_LINK_REL, TARGET_NEW_WINDOW } from '~/utilities/html'
 import type { AnchorProps } from '~/utilities/html'
-import { isInternalLink } from '~/utilities/url'
+import { isInternalLink, isMailToLink } from '~/utilities/url'
 
 const props = defineProps<AnchorProps>()
 
 const isInternal = computed(() => isInternalLink(props.href))
+const isMailTo = computed(() => isMailToLink(props.href))
 
 const sanitisedAnchorProps = computed(() => ({
   ...props,
   to: isInternal.value ? props.href : undefined, // copy `href` to `to` for vue-router usage
-  rel: props.rel ?? (!isInternal.value ? EXTERNAL_LINK_REL : undefined),
-  target: props.target ?? (!isInternal.value ? TARGET_NEW_WINDOW : undefined)
+  rel:
+    props.rel ??
+    (!isInternal.value && !isMailTo.value ? EXTERNAL_LINK_REL : undefined),
+  target:
+    props.target ??
+    (!isInternal.value && !isMailTo.value ? TARGET_NEW_WINDOW : undefined)
 }))
 </script>
