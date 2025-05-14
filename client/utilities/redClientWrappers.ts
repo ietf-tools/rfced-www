65f7273569cc2f25a1e50b59c851de3dab90b60f
Matthew Holloway <matthew@holloway.co.nz>
2025-05-15 11:58:59 +1200

    chore: redClientWrapper.ts only server and misc tidy (#62)
    * chore: ensuring redClientWrapper is only called serverside
    
    * fix: removing debug
    
    * fix: fixing markdown links
    
    * chore: fixing docstring formatting
    
    * fix: anchor links
    
    * fix: detect vitest test runner
    
    * chore: removing debug

--- client/utilities/redClientWrappers.ts
+++ client/utilities/redClientWrappers.ts
@@ -4,6 +4,9 @@ import { isProdApi } from './url'
 import { ApiClient } from '~/generated/red-client'
 
 export const getRedClient = () => {
+  const isServer = import.meta.server
+  const isTest = import.meta.env.VITEST
+
   const config = useRuntimeConfig()
   const headers: ApiClient['Config']['headers'] = {}
   const {
@@ -20,7 +23,7 @@ export const getRedClient = () => {
   }
   if (typeof datatrackerBase !== 'string') {
     throw Error(
-      'Required nuxt.config.ts runtimeConfig.public.datatrackerBase not found (or not a string)'
+      `Required nuxt.config.ts runtimeConfig.public.datatrackerBase not found (or not a string). Was typeof=${typeof datatrackerBase}. isServer=${isServer}. isTest=${isTest}`
     )
   }
 
@@ -32,7 +35,13 @@ export const getRedClient = () => {
       typeof cfServiceTokenSecret !== 'string')
   ) {
     throw Error(
-      `Detected ${datatrackerBase} as prod API but problems with required headers cfServiceTokenId=${typeof cfServiceTokenId} or cfServiceTokenSecret=${typeof cfServiceTokenSecret} `
+      `Detected ${datatrackerBase} as prod API but required headers cfServiceTokenId=${typeof cfServiceTokenId} or cfServiceTokenSecret=${typeof cfServiceTokenSecret} were missing. isServer=${isServer}. isTest=${isTest}`
+    )
+  }
+
+  if (!isServer && !isTest) {
+    throw Error(
+      `redClientWrapper should only be called serverside or in a test runner. Was isServer=${isServer}. isTest=${isTest}`
     )
   }
 
