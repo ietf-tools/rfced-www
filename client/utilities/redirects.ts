dc80dbd993d2bcffeb914d874376dbe94e8785d3
Matthew Holloway <matthew@staff.ietf.org>
2025-05-15 09:28:16 +1200

    chore: fixing docstring formatting

--- client/utilities/redirects.ts
+++ client/utilities/redirects.ts
@@ -1,7 +1,7 @@
 /**
   nuxt.config.ts can only handle basic redirects and more complex redirects need to be handled in middleware
 
-  While Nuxt does support trailing '**' patterns (see https://github.com/nitrojs/nitro/pull/1976 ) these
+  While Nuxt does support trailing `'**'` patterns (see https://github.com/nitrojs/nitro/pull/1976 ) these
   aren't expressive enough for our usecase which is to redirect /rfc/rfc*.json to /api/v1/rfc/rfc*.json
   where the * is a wildcard whose value gets included in the redirect. The nuxt.config.ts redirect wouldn't
   support wildcards when they're non-trailing, ie before the '.json' suffix.
