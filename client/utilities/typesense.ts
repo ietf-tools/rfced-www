0be30064a8859f01216340a43cc050a62b05ca06
Matthew Holloway <matthew@staff.ietf.org>
2025-05-28 12:39:39 +1200

    chore: parse typesense response using Zod

--- client/utilities/typesense.ts
+++ client/utilities/typesense.ts
@@ -1,38 +1,59 @@
 import type { Rfc } from '../generated/red-client'
+import { z } from 'zod'
 
-export type TypeSenseSearchItem = {
-  objectID: string
-  rfc: string
-  filename: string
-  title: string
-  stdlevelname: string
-  abstract: string
-  authors: { name: string }[]
-  publicationDate: number
-  groupName: string
-  areaName: string
-}
+export type TypeSenseSearchItem = z.infer<typeof TypeSenseSearchItemSchema>
+
+export const TypeSenseSearchItemSchema = z.object({
+  objectID: z.string(),
+  rfc: z.string(),
+  filename: z.string(),
+  title: z.string(),
+  stdlevelname: z.string(),
+  abstract: z.string(),
+  authors: z
+    .array(
+      z.object({
+        name: z.string()
+      })
+    )
+    .optional(),
+  publicationDate: z.number(),
+  groupName: z.string(),
+  areaName: z.string().optional()
+})
 
 export const typeSenseSearchItemToRFC = (
   typeSenseSearchItem: TypeSenseSearchItem
 ): Rfc => {
+  const result = TypeSenseSearchItemSchema.safeParse(typeSenseSearchItem)
+  if (result.error) {
+    console.error(result.error.toString())
+    throw Error(result.error.toString())
+  }
+
+  const item = result.data
+
   return {
-    number: parseInt(typeSenseSearchItem.rfc, 10),
-    title: typeSenseSearchItem.title,
-    published: new Date(typeSenseSearchItem.publicationDate).toUTCString(),
-    authors: typeSenseSearchItem.authors.map((author, index) => ({
-      person: index,
-      name: author.name
-    })),
-    area: {
-      acronym: typeSenseSearchItem.areaName,
-      name: typeSenseSearchItem.areaName
-    },
+    number: parseInt(item.rfc, 10),
+    title: item.title,
+    published: new Date(item.publicationDate).toUTCString(),
+    authors:
+      item.authors?.map((author, index) => ({
+        person: index,
+        name: author.name
+      })) ?? [],
+    area:
+      item.areaName ?
+        {
+          acronym: item.areaName,
+          name: item.areaName
+        }
+      : undefined,
     group: {
-      acronym: typeSenseSearchItem.groupName,
-      name: typeSenseSearchItem.groupName
+      acronym: item.groupName,
+      name: item.groupName
     },
-    abstract: typeSenseSearchItem.abstract,
+    abstract: item.abstract,
     status: {
       slug: 'unknown',
       name: ''
