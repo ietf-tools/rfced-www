eceae54d46512b33f04ebce0c2ffca1ff74ef3f1
NGPixel <github@ngpixel.com>
2025-06-07 20:12:45 -0400

    fix(search): predefined order for status filter

--- client/components/SearchFilterStatuses.vue
+++ client/components/SearchFilterStatuses.vue
@@ -1,9 +1,12 @@
 <template>
-  <ais-refinement-list attribute="stdlevelname" :sort-by="['name:asc']">
+  <ais-refinement-list
+    attribute="stdlevelname"
+    :sort-by="reorderItems"
+  >
     <template #default="{ items, refine }">
       <fieldset>
         <legend class="text-base font-bold mb-2">Status</legend>
-        <ul class="grid-cols-1 2xl:grid-cols-2 grid gap-1">
+        <ul class="grid-cols-1 2xl:grid-rows-4 grid gap-1 2xl:grid-flow-col 2xl:auto-cols-fr">
           <li v-for="item in items" :key="item.value">
             <label class="text-base cursor-pointer">
               <input
@@ -22,6 +25,21 @@
   </ais-refinement-list>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { AisRefinementList } from 'vue-instantsearch/vue3/es'
+
+const predefinedOrder = [
+  'Internet Standard',
+  'Proposed Standard',
+  'Draft Standard',
+  'Best Current Practice',
+  'Experimental',
+  'Historic',
+  'Informational',
+  'Unknown'
+]
+
+function reorderItems(a: { name: string }, b: { name: string }) {
+  return predefinedOrder.indexOf(a.name) - predefinedOrder.indexOf(b.name)
+}
 </script>
