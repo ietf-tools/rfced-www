53cb5ac10f91c17dd04a9c4828fa0ba9cd987e17
NGPixel <github@ngpixel.com>
2025-06-10 17:19:19 -0400

    feat(search): add support for std and fyi series

--- client/utilities/search-client-middleware.ts
+++ client/utilities/search-client-middleware.ts
@@ -8,13 +8,13 @@ export function adaptSearchClient(searchClient: TypeSenseClient): TypeSenseClien
       // ------------------------------------------
       // OVERRIDES
       // ------------------------------------------
-      // BCP query
-      const bcpMatches = r.params?.query.match(/^bcp[ :=]{0,3}(?<num>[0-9]+)\s?$/i)
+      // Series query
+      const bcpMatches = r.params?.query.match(/^(?<series>bcp|std|fyi)[ :=]{0,3}(?<num>[0-9]+)\s?$/i)
       if (bcpMatches?.groups?.num) {
         if (!r.params.facetFilters) {
           r.params.facetFilters = []
         }
-        r.params.facetFilters.push(`bcp:${bcpMatches.groups.num}`)
+        r.params.facetFilters.push(`${bcpMatches.groups.series}:${bcpMatches.groups.num}`)
         r.params.query = '*'
       }
 
