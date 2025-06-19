a19ee2d8ad80188b8e109e6bcc531c0e41954830
Matthew Holloway <matthew@staff.ietf.org>
2025-06-19 16:58:33 +1200

    chore: linting

--- client/utilities/rfc.ts
+++ client/utilities/rfc.ts
@@ -1,9 +1,4 @@
-import { DateTime } from 'luxon'
-import type { Rfc } from '../generated/red-client'
 import { NONBREAKING_SPACE } from './strings'
-import { infoRfcPathBuilder, PUBLIC_SITE } from './url'
-import { FIXME_getRFCWithMissingData } from './rfc.mocks'
-import { formatAuthor, formatIdentifiers } from './rfc-converters-utils'
 
 export type RfcCommon = {
   number: number
@@ -159,18 +154,6 @@ export const formatTitleAsVNode = (rfcId: string) => {
     NONBREAKING_SPACE,
     h('span', { class: 'font-bold' }, parts.number)
   ])
-}
-
-/**
- * Renders RFC summary txt. Eg.
- *
- * Crocker, S., "Host Software", RFC 1, DOI 10.17487/RFC0001, April 1969, <https://www.rfc-editor.org/info/rfc1>.
- *
- * As used on https://www.rfc-editor.org/refs/ref0001.txt
- */
-export const refsRefRfcIdTxt = (rfc: Rfc): string => {
-  const rfcWithMissingData = FIXME_getRFCWithMissingData(rfc)
-  return `${rfcWithMissingData.authors.map((author) => formatAuthor(author, 'brief'))}, "${rfcWithMissingData.title}", RFC ${rfcWithMissingData.number}, ${formatIdentifiers(rfcWithMissingData.identifiers, ' ').join('')}, ${DateTime.fromISO(rfcWithMissingData.published).toFormat('LLLL yyyy')}, <${PUBLIC_SITE}${infoRfcPathBuilder(`rfc${rfcWithMissingData.number}`)}>.\n`
 }
 
 export type RFCJSON = {
