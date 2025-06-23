28b39cc4c975e60c73519e51feaae526f6f0ed62
Matthew Holloway <matthew@staff.ietf.org>
2025-06-23 19:00:42 +1200

    feat: Mobile search

--- client/components/SearchMobileFilter.vue
+++ client/components/SearchMobileFilter.vue
@@ -1,13 +1,15 @@
 <template>
   <DialogRoot v-model:open="isOpen">
     <DialogTrigger>
-      <button
-        type="button"
-        class="flex justify-between w-full py-2 px-3 items-center border-1 border-gray-400 font-bold"
-        @click="isOpen = false"
-      >
-        Filter RFCs
-      </button>
+      <span class="block mr-4">
+        <button
+          type="button"
+          class="flex justify-between w-full py-2 px-3 items-center border-1 border-gray-400 font-bold"
+          @click="isOpen = false"
+        >
+          Filter RFCs
+        </button>
+      </span>
     </DialogTrigger>
     <DialogPortal>
       <DialogOverlay />
@@ -23,16 +25,35 @@
           </DialogClose>
         </DialogTitle>
         <DialogDescription>
-          <div class="flex flex-col">
-            <SearchFilter />
+          <div class="flex flex-col mb-20">
+            <div class="ml-5 mb-3">
+              <SearchInRfcComments />
+            </div>
+            <fieldset class="ml-5 mr-5">
+              <legend class="font-bold">UI Density</legend>
+              <div class="flex items-center lg:h-10">
+                <SearchDensity v-model="searchStore.density" />
+              </div>
+            </fieldset>
+            <div class="pr-5">
+              <SearchFilter />
+            </div>
           </div>
           <div
-            class="flex flex-row justify-between mt-4 border-t border-t-gray-200 w-full px-2 py-2"
+            class="fixed bottom-0 flex flex-row justify-between mt-4 border-t border-t-gray-200 w-full"
           >
-            TODO
+            <button
+              type="button"
+              @click="isOpen = false"
+              class="border-1 w-full font-bold bg-black text-white px-2 py-2"
+            >
+              Ok
+            </button>
           </div>
         </DialogDescription>
-        <DialogClose />
+        <DialogClose class="fixed top-4 right-5">
+          <GraphicsClose class="text-black" />
+        </DialogClose>
       </DialogContent>
     </DialogPortal>
   </DialogRoot>
@@ -51,4 +72,6 @@ import {
 } from 'reka-ui'
 
 const isOpen = ref(false)
+
+const searchStore = useSearchStore()
 </script>
--- client/pages/search.vue
+++ client/pages/search.vue
@@ -44,14 +44,7 @@
                 </ais-search-box>
               </div>
               <div class="hidden md:block pl-5 grow">
-                <label class="text-base cursor-pointer flex items-center">
-                  <input
-                    v-model="searchStore.searchContents"
-                    class="mr-1 size-5 scheme-light dark:scheme-dark"
-                    type="checkbox"
-                  />
-                  <span>Search in RFC contents</span>
-                </label>
+                <SearchInRfcComments />
               </div>
             </div>
           </div>
